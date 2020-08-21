e6039e3e2ab6e58b08b7be010f7be693fe0524df
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-21 09:48:20 -0700

    add TDD parameters

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 // Test Driven Development:
-  // Test:
-  // Input:
-  // Output:
+  // Test: returns a range of numbers from 0 to the users inputted number.
+  // Input: 5
+  // Output: "0, 1, 2, 3, 4, 5"
+  
+  // Test: splits the user inputed number from a string of numbers to a range of individual strings.
+  // Input: 5
+  // Output: "1","2","3","4","5"
 
-  // Test:
-  // Input:
-  // Output:
+  // Test: will identfy all "3" and convert them to "Won't you be my neighbor?".
+  // Input: 13
+  // Output: "1","2","Won't you be my neighbor?","4","..."12", "Won't you be my neighbor?"
 
-  // Test:
-  // Input:
-  // Output:
+  // Test: will identfy "12" and convert it to "boop"
+  // Input: 12
+  // Output: "1","2","3"..."10","11","Boop"
 
-  // Test:
-  // Input:
-  // Output:
+  // Test: will identfy all "2" and convert them to "boop" unless the inputed number includes a combinded number such as "23" or "32".
+  // Input: 12345
+  // Output: "1","Boop","3","4","5"
 
-  // Test:
+  // Test: will identfy all "1" and convert them to "beep", unless the inputed number includes combinded number such as "12" or "13".
+  // Input: 5
+  // Output: "Beep","2","3","4","5"
+
+  // Test: will allow user to clear results and imput a new number
   // Input:
   // Output:
 
